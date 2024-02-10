9f712f2b558b1887ac9de8ffce099223922254aa
M. J. Fromberger <michael.j.fromberger@gmail.com>
2024-02-10 12:32:40 -0800

    Export the VersionInfo type and add a constructor.
    Allow the caller access to the same version information the VersionCommand
    constructor uses.

--- version.go
+++ version.go
@@ -5,7 +5,6 @@ package command
 import (
 	"encoding/hex"
 	"encoding/json"
-	"errors"
 	"flag"
 	"fmt"
 	"os"
@@ -27,37 +26,7 @@ func VersionCommand() *C {
 			fs.BoolVar(&doJSON, "json", false, "Write version information as JSON")
 		},
 		Run: Adapt(func(env *Env) error {
-			bi, ok := debug.ReadBuildInfo()
-			if !ok {
-				return errors.New("no version information is available")
-			}
-			vi := versionInfo{
-				Name:      filepath.Base(os.Args[0]),
-				Path:      bi.Path,
-				Toolchain: bi.GoVersion,
-			}
-			vi.parseModule(&bi.Main)
-
-			// Check for build settings. These may not be present if the build was done
-			// without the repository available, e.g., via go install outside a module.
-			// If these settings are present they are preferred.
-			for _, s := range bi.Settings {
-				switch s.Key {
-				case "vcs.revision":
-					vi.Commit = s.Value
-				case "vcs.time":
-					ts, err := time.Parse(time.RFC3339, s.Value)
-					if err == nil {
-						vi.Time = &ts
-					}
-				case "vcs.modified":
-					vi.Modified = s.Value == "true"
-				case "GOOS":
-					vi.OS = s.Value
-				case "GOARCH":
-					vi.Arch = s.Value
-				}
-			}
+			vi := GetVersionInfo()
 			if doJSON {
 				json.NewEncoder(os.Stdout).Encode(vi)
 				return nil
@@ -68,23 +37,84 @@ func VersionCommand() *C {
 	}
 }
 
-// versionInfo records the version information extracted from the build info
-// record by the "version" command implementation.
-type versionInfo struct {
-	Name      string     `json:"name"`
-	Path      string     `json:"path,omitempty"`
-	Version   string     `json:"version,omitempty"`
-	Commit    string     `json:"commit,omitempty"`
-	Modified  bool       `json:"modified,omitempty"`
-	Toolchain string     `json:"toolchain,omitempty"`
-	OS        string     `json:"os,omitempty"`
-	Arch      string     `json:"arch,omitempty"`
-	Time      *time.Time `json:"time,omitempty"`
+// VersionInfo records version information extracted from the build info record
+// for the running program.
+type VersionInfo struct {
+	// Name is the base name of the running binary from os.Args.
+	Name string `json:"name"`
+
+	// Path is the import path of the main package.
+	Path string `json:"path"`
+
+	// Version, if available, is the version tag at which the binary was built.
+	// This is empty if no version label is available, e.g. an untagged commit.
+	Version string `json:"version,omitempty"`
+
+	// Commit, if available, is the commit hash at which the binary was built.
+	// Typically this will be a hex string, but the format is not guaranteed.
+	Commit string `json:"commit,omitempty"`
+
+	// Modified reports whether the contents of the build environment were
+	// modified from a clean state. This may indicate the presence of extra
+	// files in the working directory, even if the repository is up-to-date.
+	Modified bool `json:"modified,omitempty"`
+
+	// Toolchain gives the Go toolchain version that built the binary.
+	Toolchain string `json:"toolchain"`
+
+	// OS gives the GOOS value used by the compiler.
+	OS string `json:"os,omitempty"`
+
+	// Arch gives the GOARCH value used by the compiler.
+	Arch string `json:"arch,omitempty"`
+
+	// Time, if non-nil, gives the timestamp corresponding to the commit at
+	// which the binary was built.  It is nil if the commit time is not
+	// recorded; otherwise the value is a non-zero time in UTC.
+	Time *time.Time `json:"time,omitempty"`
 }
 
-// String encodes the versionInfo in a single-line human-readable format.
-// This is used for the plain "version" output.
-func (v versionInfo) String() string {
+// GetVersionInfo returns a VersionInfo record extracted from the build
+// metadata in the currently running process. If no build information is
+// available, only the Name field will be populated.
+func GetVersionInfo() VersionInfo {
+	bi, ok := debug.ReadBuildInfo()
+	if !ok {
+		return VersionInfo{Name: filepath.Base(os.Args[0])}
+	}
+	vi := VersionInfo{
+		Name:      filepath.Base(os.Args[0]),
+		Path:      bi.Path,
+		Toolchain: bi.GoVersion,
+	}
+	vi.parseModule(&bi.Main)
+
+	// Check for build settings. These may not be present if the build was done
+	// without the repository available, e.g., via go install outside a module.
+	// If these settings are present they are preferred.
+	for _, s := range bi.Settings {
+		switch s.Key {
+		case "vcs.revision":
+			vi.Commit = s.Value
+		case "vcs.time":
+			ts, err := time.Parse(time.RFC3339, s.Value)
+			if err == nil {
+				vi.Time = &ts
+			}
+		case "vcs.modified":
+			vi.Modified = s.Value == "true"
+		case "GOOS":
+			vi.OS = s.Value
+		case "GOARCH":
+			vi.Arch = s.Value
+		}
+	}
+	return vi
+}
+
+// String encodes v in a single-line human-readable format.  This is the format
+// used for plain text output by the "version" command implementation.
+func (v VersionInfo) String() string {
 	var sb strings.Builder
 	sb.WriteString(v.Name)
 	if v.Version != "" {
@@ -111,7 +141,7 @@ func (v versionInfo) String() string {
 // parseModule reports whether m contains version and commit information and,
 // if so, populates the corresopnding fields of v.  If the module has a replace
 // directive, the replacement is preferred.
-func (v *versionInfo) parseModule(m *debug.Module) bool {
+func (v *VersionInfo) parseModule(m *debug.Module) bool {
 	if m.Replace != nil && v.parseModule(m.Replace) {
 		return true
 	}
@@ -123,8 +153,10 @@ func (v *versionInfo) parseModule(m *debug.Module) bool {
 		v.Time = &ts
 		return true
 	}
-	v.Version = m.Version
-	return m.Version != ""
+	if m.Version != "(devel)" {
+		v.Version = m.Version
+	}
+	return v.Version != ""
 }
 
 // parsePseudoVersion reports whether s appears to be a Go module pseudoversion
