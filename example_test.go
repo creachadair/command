65dfae3971b7c9e6d835a6034680a5677ed4ec55
M. J. Fromberger <michael.j.fromberger@gmail.com>
2025-01-09 14:10:16 -0800

    Clean up the test example.

--- example_test.go
+++ example_test.go
@@ -3,31 +3,31 @@ package command_test
 import (
 	"flag"
 	"fmt"
-	"io"
-	"log"
 	"os"
 	"strings"
 
 	"github.com/creachadair/command"
 )
 
-func Example() {
-	// The environment passed to a command can carry an arbitrary config value.
-	// Here we use a struct carrying information about options.
-	type options struct {
-		noNewline bool
-		label     string
-		private   int
-		confirm   bool
-	}
+// The environment passed to a command can carry an arbitrary config value.
+// Here we use a struct carrying information about options.
+type options struct {
+	noNewline bool
+	label     string
+	private   int
+	confirm   bool
+}
 
+func Example() {
 	root := &command.C{
 		Name: "example",
 
-		// Usage may have multiple lines, and can omit the command name.
-		Usage: "command args...",
+		// Usage may have multiple lines, each describing a different mode of
+		// operation. The command name can be omitted; the help printer will add
+		// it if it is not present.
+		Usage: "command args...\nhelp",
 
-		// The first line of the help text is used as "short" help.
+		// The first line of help text is used as a synopsis in help listings.
 		// Any subsequent lines are include in "long" help.
 		Help: `Do interesting things with arguments.
 
@@ -46,20 +46,8 @@ This help text is printed by the "help" subcommand.`,
 		// Executing it without a subcommand will print a help message and exit
 		// with error.
 
+		// Subcommands.
 		Commands: []*command.C{
-			// This function creates a basic "help" command that prints out
-			// command help based on usage and help strings.
-			//
-			// This command can also have "help topics", which are stripped-down
-			// commands with only help text (no flags, usage, or other behavior).
-			command.HelpCommand([]command.HelpTopic{{
-				Name: "special",
-				Help: "This is some useful information a user might care about.",
-			}, {
-				Name: "magic",
-				Help: `The user can write "command help <topic>" to get this text.`,
-			}}),
-
 			// This is a typical user-defined command.
 			{
 				Name:  "echo",
@@ -108,69 +96,25 @@ This help text is printed by the "help" subcommand.`,
 				// Demonstrate that a command which panics is properly handled.
 				Run: func(env *command.Env) error { panic("ouch") },
 			},
+
+			// This function creates a basic "help" command that prints out
+			// command help based on usage and help strings.
+			//
+			// This command can also have "help topics", which are stripped-down
+			// commands with only help text (no flags, usage, or other behavior).
+			command.HelpCommand([]command.HelpTopic{{
+				Name: "special",
+				Help: "This is some useful information a user might care about.",
+			}, {
+				Name: "magic",
+				Help: `The user can write "command help <topic>" to get this text.`,
+			}}),
 		},
 	}
 
-	// For purposes of the test output, discard help output.
-	null, err := os.Create("/dev/null")
-	if err != nil {
-		log.Fatal(err)
-	}
-	save := os.Stdout
-	os.Stdout = null
-
-	// Demonstrate help output.
-	//
-	// Note that the argument to NewEnv is plumbed via the Config field of Env.
-
-	var opt options
-	env := root.NewEnv(&opt)
-	env.Log = io.Discard
-
-	command.Run(env, []string{"help"})
-	opt = options{}  // reset settings
-	os.Stdout = save // restore stdout
-
-	// Requesting help for an unlisted subcommand reports an error.
-	command.Run(env, []string{"help", "secret"})
-	opt = options{}
-
-	// But if you name the command explicitly with -help, you get help.
-	command.Run(env, []string{"secret", "-help"})
-	opt = options{}
-
-	// Execute a command with some arguments.
-	command.RunOrFail(env, []string{"echo", "foo", "bar"})
-	opt = options{}
-
-	// Execute a command with some flags and argujments.
-	command.RunOrFail(env, []string{"-label", "xyzzy", "echo", "bar"})
-	opt = options{}
-
-	// Merged flags can be used anywhere in their scope.
-	command.RunOrFail(env, []string{"echo", "-label", "foo", "bar"})
-	opt = options{}
-
-	// Private-marked flags still work as expected.
-	command.RunOrFail(env, []string{"echo", "-p", "25", "-label", "ok", "bar"})
-	opt = options{}
-
-	// Executing an unlisted command works.
-	command.RunOrFail(env, []string{"secret", "fort"})
-	opt = options{}
-
-	// A command that panics is caught and reported as an error.
-	fmt.Println("[err]", command.Run(env, []string{"fatal"}))
-
-	// An unmerged flag.
-	command.RunOrFail(env, []string{"echo", "-n", "baz"})
-
-	// Output:
-	// foo bar
-	// [xyzzy] bar
-	// [foo] bar
-	// [ok] <25> bar
-	// easter-egg fort
-	// [err] command "fatal" panicked: ouch
-	// baz
+	// To run a comment, we need to construct an environment for it.
+	// The environment can carry an arbitrary config value, which in this case
+	// is our custom options type.
+	var opts options
+	command.RunOrFail(root.NewEnv(&opts), os.Args[1:])
 }
